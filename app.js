d7984d9f7ce163a3b5c205cdeb8827bf62132989
Saniya98 <saniyafarheen.98@gmail.com>
2024-04-01 11:29:39 -0400

    Undo revert of commit 00a00c6

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 
 const express=require('express');
-const expressLayout = require('express-ejs-layouts');
-
-const connectDB=require('./server/config/db');
+const expressLayout = require('express-ejs-layouts')
 
 const app=express();
 const PORT=5000 || process.env.PORT;
 
-connectDB();
 
 app.use(express.static('public'));
 
@@ -18,7 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use('/',require('./server/routes/main'));
 app.use('/about',require('./server/routes/main'));
-app.use('/contact',require('./server/routes/main'));
+app.use('/contact',require('./server/routes/main'))
 
 
 app.listen(PORT, ()=>{
