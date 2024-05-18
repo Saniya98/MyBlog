15252e1fb4f41d2557acb596632c87e394da3aaa
Saniya98 <saniyafarheen.98@gmail.com>
2024-05-17 20:53:18 -0400

    Implemented admin form page

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
+const adminLayout = '../views/layouts/admin'
+/*
+ * GET /
+ * Admin - login page
+ */
+router.get("/admin", async (req, res) => {
+  try {
+    const locals = {
+      title: "Admin",
+      desciption: "This is an admin page",
+    };
+    res.render('admin/index', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get('/admin',async(req,res)=>{
-    try{
-        const locals={
-            title:"admin",
-            desciption:"This is an admin page"
-        }
-        res.render('admin/index', {locals})
-    }catch(error){
-        console.log("error")
-    }
-})
+module.exports = router;
