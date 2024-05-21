060072231fd3980cdc8b71b2aff56d14cc16eed7
Saniya98 <saniyafarheen.98@gmail.com>
2024-05-20 23:20:52 -0400

    Implemented JWT authorization

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,11 +2,31 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
 const User = require("../models/user");
-const bcrypt = require ('bcrypt');
-const jwt= require('jsonwebtoken');
-const jwtSecret = process.env.JWT_SECRET
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
-const adminLayout = '../views/layouts/admin'
+const adminLayout = "../views/layouts/admin";
+
+/*
+ *
+ * Check Login
+ */
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch(error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
 /*
  * GET /
  * Admin - login page
@@ -17,7 +37,7 @@ router.get("/admin", async (req, res) => {
       title: "Admin",
       desciption: "This is an admin page",
     };
-    res.render('admin/index', { locals, layout: adminLayout });
+    res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
@@ -30,59 +50,55 @@ router.get("/admin", async (req, res) => {
 // router.post("/admin", async (req, res) => {
 //     try {
 //      const{username, password}=req.body;
-    
+
 //     } catch (error) {
 //       console.log(error);
 //     }
 //   });
 
-  router.post("/admin", async (req, res) => {
-    try {
-     const{username, password}=req.body;
-    
-    const user = await User.findOne({username})
-    if(!user){
-      return res.status(401).json({message: 'Invalid Credentials'})
+router.post("/admin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Credentials" });
     }
 
-    const passwordIsValid = await bcrypt.compare(password, user.password)
-    if(!passwordIsValid){
-      return res.status(401).json({message: 'Invalid Credentials'})
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).json({ message: "Invalid Credentials" });
     }
 
-    const token = jwt.sign({userId:user._id},jwtSecret)
-    res.cookie('token', token, {httpOnly: true})
-    res.redirect('/dashboard');
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-    } catch (error) {
-      console.log(error);
-    }
-  });
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  res.render("admin/dashboard");
+});
 
-  router.get('/dashboard',(req,res)=>{
-    res.render('admin/dashboard')
-  })
-
-
-  /*
+/*
  * POST /
  * Admin - Register
  */
 router.post("/register", async (req, res) => {
   try {
-   const{username, password}=req.body;
-   const hashedPassword= await bcrypt.hash(password, 10)
-   try{
-    const user = await User.create({username,password: hashedPassword})
-    res.status(201).json({message:'User Created', user})
-   }catch(error){
-    if(error.code===11000){
-      res.status(409).json({message:'user already in use'})
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User Created", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "user already in use" });
+      }
+      res.status(500).json({ message: "Internal server error" });
     }
-    res.status(500).json({message:'Internal server error'})
-   }
-
-
   } catch (error) {
     console.log(error);
   }
